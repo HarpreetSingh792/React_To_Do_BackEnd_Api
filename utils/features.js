3d5c31122e15e4f19339779733c0528183a2ee59
Harpreet Singh <110280351+HarpreetSingh792@users.noreply.github.com>
2023-07-20 01:39:42 +0530

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -1,10 +1,8 @@
 import jwt from "jsonwebtoken";
 
-
 export const jwtToken = (user, res, message, statusCode = 200) => {
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
     res.status(statusCode).cookie("token", token, { maxAge: (15 * 60 * 1000), httpOnly: true, sameSite: "none", secure: true}).json({
-        token,
         success: true,
         message: message
 
