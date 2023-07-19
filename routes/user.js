44ed5019306018c4ffae83fd732bd828edc9ec25
Harpreet Singh <110280351+HarpreetSingh792@users.noreply.github.com>
2023-07-20 02:23:45 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ import {userDetails,userRegister,userLogin} from '../controllers/user.js';
 import  isAuthenticated  from "../middlewares/authenticated.js";
 const router = express.Router();
 
-
+router.get("/logout",isAuthenticated,Logout)
 router.get("/me", isAuthenticated ,userDetails)
 router.post("/login", userLogin)
 router.post("/register", userRegister)
