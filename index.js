8a6fd701d1b45f599ad68f239e15abee4eb62be7
Harpreet Singh <110280351+HarpreetSingh792@users.noreply.github.com>
2023-07-21 02:22:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin:process.env.FrontEnd_URL,
-    methods:['GET','POST','PUT','DELTE'],
+    methods:['GET','POST','PUT','DELETE'],
     credentials:true,
 }))
 
